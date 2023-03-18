78bff1fdd93b8c4e71cb4dd55266f2c793aee26c
sava <sava@DESKTOP-CJS7LGO>
2023-03-18 11:40:06 +0300

    Добавьте файлы проекта.

---
+++ SF2022User{NN}Lib/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SF2022User_NN_Lib
+{
+    public class Calculations
+    {
+        /// <summary>
+        /// Библиотека классов для возрата списка свободных временных интервалов
+        /// </summary>
+        /// <summary>
+        /// Метод для возрата списка свободных временных интервалов
+        /// </summary>
+        /// <param name="startTimes"></param>
+        /// <param name="durations"></param>
+        /// <param name="beginWorkingTime"></param>
+        /// <param name="endWorkingTime"></param>
+        /// <param name="consultationTime"></param>
+        /// <returns></returns>
+        public static string[] AvailablePeriods(TimeSpan[] startTimes, int[] durations, TimeSpan beginWorkingTime, TimeSpan endWorkingTime, int consultationTime)
+        {
+            string[] arr = {"f"};
+            return arr;
+        }
+
+      
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc7023e9-1f7d-48a0-a036-66004e1e3c89")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
